19e74995a09117712d2ac2c69fd51681589cf2d8
Tom Potter <tp10g13@soton.ac.uk>
2015-11-21 13:08:55 +0000

    added check in page

--- main.py
+++ main.py
@@ -18,8 +18,18 @@ def user_key(user_name="Test"):
 class User(ndb.Model):
 	username=ndb.StringProperty()
 
+class LectureCode(ndb.Model):
+	lecture=ndb.StringProperty()
+	code=ndb.StringProperty()
+
+class CheckIn(ndb.Model):
+	student=ndb.StringProperty()
+	lecture=ndb.StringProperty()
 
 
+template_values = {
+			
+}
 
 class LoginPage(webapp2.RequestHandler):
 	def get(self):
@@ -31,40 +41,61 @@ class LoginPage(webapp2.RequestHandler):
 		user = User(parent=user_key('User'))
 
 		user.username=self.request.get('username')
+
+		template_values['username'] = user.username
+
 		user.put()
 		self.redirect('/home')
 
 
-
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		
-		template_values = {
-			
-		}
 
-		query = User.query(User.username == 'test1')
-		for user in query:
-			 template_values['username'] = user.username
+		userQuery = User.query(User.username == "example1")
+		for user in userQuery:
+			template_values['example'] = user.username
 
 		template = JINJA_ENVIRONMENT.get_template('/assets/home.html')
 		self.response.write(template.render(template_values))
 
-		
+
+class CheckInPage(webapp2.RequestHandler):
+	def get(self):
+		template = JINJA_ENVIRONMENT.get_template('/assets/checkin.html')
+		self.response.write(template.render())
+
+	def post(self):
+		code = self.request.get('checkincode')		
+		if(code == "test"):
+			checkin = CheckIn()
+			checkin.student=template_values['username']
+			checkin.lecture=self.request.get('checkinlecture')
+			checkin.put()
+			self.redirect('/home')
+		else:
+			self.response.write("invalid code")
+
 
 class ChallengesPage(webapp2.RequestHandler):
 	def get(self):
 		template = JINJA_ENVIRONMENT.get_template('/assets/challenges.html')
 		self.response.write(template.render())
 
+	def post(self):
+		challenge1 = self.response.get("chall1")
+		self.response.write(challenge1)
+
+
 class HistoryPage(webapp2.RequestHandler):
 	def get(self):
 		template = JINJA_ENVIRONMENT.get_template('/assets/history.html')
 		self.response.write(template.render())
+		
 
 app = webapp2.WSGIApplication([
 	('/', LoginPage),
 	('/home', MainPage),
+	('/checkin', CheckInPage),
 	('/challenges', ChallengesPage),
 	('/history', HistoryPage)
 ], debug=True)
